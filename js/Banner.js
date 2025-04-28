18687c67e19fa6d0025428a3050406053d013fdd
Baljeat_Les <hlan9693@gmail.com>
2025-04-28 18:38:52 +0700

    Ver1

--- js/Banner.js
+++ js/Banner.js
@@ -2,9 +2,10 @@ $(document).ready(function(){
   $('.slider').slick({
     infinite:true,
     autoplay:true,
-    autoplaySpeed:4000,
-    speed:2000,
-    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    autoplaySpeed:200,
+    speed:2000
+    ,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-angle-left' aria-hidden='true'></i></button>",
     nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
 
   })
